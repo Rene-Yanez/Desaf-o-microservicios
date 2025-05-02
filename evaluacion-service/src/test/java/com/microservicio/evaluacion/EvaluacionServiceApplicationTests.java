b72b9365ee3a73d2f6500565dd40977edf2c61ca
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2025-05-02 16:02:32 -0400

    Final

---
+++ evaluacion-service/src/test/java/com/microservicio/evaluacion/EvaluacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
